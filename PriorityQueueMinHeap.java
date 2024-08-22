26e2b80235875bc9457e561d03544bb5239171b9
himanshuraghav662 <108710259+himanshuraghav662@users.noreply.github.com>
2024-08-22 21:15:51 +0530

    Create PriorityQueueMinHeap.java

---
+++ PriorityQueueMinHeap.java
@@ -0,0 +1,17 @@
+import java.util.PriorityQueue;
+
+public class priorityqueue {
+    public static void main(String[] args) {
+        PriorityQueue<Integer>min=new PriorityQueue<>();
+        min.add(50);
+        min.add(8);
+        min.add(20);
+        min.add(9);
+        System.out.println("priority Queue"+min);
+        System.out.println(min.peek());
+        System.out.println(min.poll());
+        System.out.println("priority Queue"+min);
+
+
+    }
+}
